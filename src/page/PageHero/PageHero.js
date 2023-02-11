c4d91d6c1f229b2b93aa71d5979df287699d1476
Anastasya <anasty22@gmail.com>
2023-02-11 13:33:08 +0200

    add scroll id

--- src/page/PageHero/PageHero.js
+++ src/page/PageHero/PageHero.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import flash from '../../images/flash.png'
 import {ContainerHero,HeaderHero,HeaderSecondHero,ImageHero} from "./PageHero.style.js"
+import { Link, animateScroll as scroll } from "react-scroll";
 
 
 const PageHero = () => {
@@ -12,10 +13,17 @@ const PageHero = () => {
 <HeaderSecondHero>
     Місце зустрічі допомоги військовим
 </HeaderSecondHero>
+<Link
+    activeClass="active"
+    to="section1"
+    spy={true}
+    smooth={true}
+    offset={-70}
+    duration={500}
+>
+<ImageHero src={flash} >
 
-<ImageHero src={flash}>
-
-</ImageHero>
+</ImageHero></Link>
       </ContainerHero>
     </div>
   )
--- src/page/PageInfo/PageInfo.js
+++ src/page/PageInfo/PageInfo.js
@@ -1,20 +1,36 @@
 import React from "react";
-import { HeaderInfo,ContainerInfo,SpanInfo,HeaderInfoAdd,ContainerAdditional } from "./PageInfo.style.js";
+import {
+  HeaderInfo,
+  ContainerInfo,
+  SpanInfo,
+  HeaderInfoAdd,
+  ContainerAdditional,
+} from "./PageInfo.style.js";
 
 const PageInfo = () => {
   return (
     <ContainerInfo>
-      <HeaderInfo>
+      <HeaderInfo id="section1">
         Військові побратими-це фонд, який займається допомогою за потребою.
         Обов"язок кожного українця-це поміч та плече підтримки людям, які
         захищають нашу Батьківщину. В пустелі одна крапля води має значення. У
         фонді кожна гривня-це крок вирішення потреб побратимів.
       </HeaderInfo>
-<ContainerAdditional>
-      <HeaderInfoAdd><SpanInfo>40+  </SpanInfo>  військовим допомогли</HeaderInfoAdd>
-      <HeaderInfoAdd><SpanInfo>50000+  </SpanInfo> гривень витрачено</HeaderInfoAdd>
-      <HeaderInfoAdd><SpanInfo>10+  </SpanInfo> залучено спеціалістів</HeaderInfoAdd>
+      <ContainerAdditional>
+        <HeaderInfoAdd>
+          <SpanInfo>40+ </SpanInfo> військовим допомогли
+        </HeaderInfoAdd>
+        <HeaderInfoAdd>
+          <SpanInfo>50000+ </SpanInfo> гривень витрачено
+        </HeaderInfoAdd>
+        <HeaderInfoAdd>
+          <SpanInfo>10+ </SpanInfo> залучено спеціалістів
+        </HeaderInfoAdd>
       </ContainerAdditional>
+      <HeaderInfo>
+        І це тільки початок роботи фонду "Військові побратими". У перспективі
+        плануються розширення меж та збільшення обсягів помічі військовим.
+      </HeaderInfo>
     </ContainerInfo>
   );
 };
